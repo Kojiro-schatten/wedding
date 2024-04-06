f5b43a205ec494b8551aa231bc2284fd3feb5163
KojiroSchatten <kouschatten@gmail.com>
2024-04-06 20:28:19 +0900

    add: pikachu

--- src/app/packages/ui/Components/Greeting/Greeting.tsx
+++ src/app/packages/ui/Components/Greeting/Greeting.tsx
@@ -1,4 +1,4 @@
-import { Box, Title } from "@mantine/core"
+import { Box, Title, Image } from "@mantine/core"
 import styles from "./Greeting.module.css"
 
 export function Greeting() {
@@ -11,6 +11,10 @@ export function Greeting() {
           - ご挨拶 -
         </Title>
       </Box>
+      <Box w={60} ml={'auto'} mt={-60}>
+        <Image src="/greeting.gif" alt="Loading"></Image>
+      </Box>
+
       <div className={styles.description}>
         謹啓 清明の候皆様にはますますご清祥のこととお慶び申し上げます。<br />このたび 私たちは結婚式を挙げることになりました<br />つきましては 日ごろお世話になっている皆様に お集まりいただき<br />ささやかな披露宴を催したいと存じます<br />ご多用中 誠に恐縮ではございますが<br />ご来臨の栄を賜りたく 謹んでご案内申し上げます。
       </div>
